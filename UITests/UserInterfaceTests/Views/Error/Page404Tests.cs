5f4270ea7dbe1c830cb4f3ceaedac9fb48bdb6ee
Troy Osland <oslandt@seattleu.edu>
2018-06-09 16:50:04 -0700

    finished the 404 tests

--- UITests/UserInterfaceTests/Views/Error/Page404Tests.cs
+++ UITests/UserInterfaceTests/Views/Error/Page404Tests.cs
@@ -23,8 +23,8 @@ namespace UserInterfaceTests.Views.Error
     {
         //these two string are used to match these tests to this 
         //  controller/action pair
-        public static string _controller = Extensions.HomeControllerName;
-        public static string _action = "About";
+        public static string _controller = "Error";
+        public static string _action = "Page404";
 
         [ClassInitialize]
         public static void MyClassInitialize(TestContext context)
@@ -48,11 +48,25 @@ namespace UserInterfaceTests.Views.Error
         }
 
         [TestMethod]
-        public void Home_About_ClickAllLinks_NoLogin()
+        public void Error_Page404_ClickAllLinks_NoLogin()
         {
             Assert.AreEqual(true, true);//replace this with links if any become available on page
         }
 
-        
+        //a test for getting to the page by bad url AKA 404 error
+        // This is essentially just plugging in a bad name to make sure my page displays
+        // and not the default 404 page
+        [TestMethod]
+        public void Error_Page404_DisplaysAfter404()
+        {
+            //navigate to bad page
+            AssemblyFile.driver.Navigate().GoToUrl(Extensions.BaseUrl + "/aslkdfdslkfn");
+
+            //check that the page is my 404 page
+            var PageResult = AssemblyFile.driver.FindElement(By.Id("Page-Done"));
+            var AreaResult = AssemblyFile.driver.FindElement(By.Id("Area--Done"));
+            var ControllerResult = AssemblyFile.driver.FindElement(By.Id("Controller-" + _controller + "-Done"));
+            var ViewResult = AssemblyFile.driver.FindElement(By.Id("View-" + _action + "-Done"));
+        }
     }
 }
