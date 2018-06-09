8b4344bab01ed024e6e411fd13547583c20f376e
Troy Osland <oslandt@seattleu.edu>
2018-06-09 12:29:41 -0700

    added ui tests for the about page

---
+++ UITests/UserInterfaceTests/Views/Home/AboutTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UserInterfaceTests.Views.Home
+{
+    [TestClass]
+    public class AboutTests
+    {
+        //these two string are used to match these tests to this 
+        //  controller/action pair
+        public static string _controller = Extensions.HomeControllerName;
+        public static string _action = "About";
+
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext context)
+        {
+            //start by naviagating to the controller/action view page
+            Extensions.NavigateToPage(AssemblyFile.driver, _controller, _action);
+
+            //now check that navbar works
+            Views.Shared.Layout.NavBar_ClickAllLinks(AssemblyFile.driver, _controller, _action);
+
+            //now check that footer works
+            Views.Shared.Layout.Footer_ClickAllLinks(AssemblyFile.driver, _controller, _action);
+        }
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            //start every test by naviagating to the controller/action view 
+            //  page
+            Extensions.NavigateToPage(AssemblyFile.driver, _controller, _action);
+        }
+
+        [TestMethod]
+        public void Home_About_ClickAllLinks_NoLogin()
+        {
+            Assert.AreEqual(true, true);//replace this with links if any become available on page
+        }
+    }
+}
