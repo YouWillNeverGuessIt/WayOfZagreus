6161096df13b127811688354e6f904dd4f9d3f6d
Troy Osland <oslandt@seattleu.edu>
2018-10-25 01:07:30 -0700

    added nuget package for mvc for controller tests

---
+++ IntegrationTests/Controllers/TimelineControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DotNetAppSqlDb.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace IntegrationTests.Controllers
+{
+    [TestClass]
+    public class TimelineControllerTests
+    {
+        public TestContext TestContext { get; set; }
+
+        #region IndexRegion
+        [TestMethod]
+        public void Controller_Home_Index_Default_Should_Pass()
+        {
+            // Arrange
+            TimelineController controller = new TimelineController();
+
+            var test = controller.Index();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+            Assert.IsNotNull(test, TestContext.TestName);
+        }
+
+        #endregion IndexRegion
+
+
+    }
+}
