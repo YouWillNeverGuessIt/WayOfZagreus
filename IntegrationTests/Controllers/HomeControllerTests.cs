730cab2b2e9fa2b3933ce5578857e44c5b55b678
Troy Osland <oslandt@seattleu.edu>
2018-10-25 12:47:56 -0700

    Updated Home Controller tests to 100% coverage

--- IntegrationTests/Controllers/HomeControllerTests.cs
+++ IntegrationTests/Controllers/HomeControllerTests.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DotNetAppSqlDb.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IntegrationTests.Controllers
 {
-    class HomeControllerTests
+    [TestClass]
+    public class HomeControllerTests
     {
+        public TestContext TestContext { get; set; }
+
+        #region Index
+        [TestMethod]
+        public void Controller_Home_Index_Default_Should_Pass()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+        #endregion
+
+        #region About
+        [TestMethod]
+        public void Controller_Home_About_Default_Should_Pass()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result, TestContext.TestName);
+        }
+        #endregion
     }
 }
